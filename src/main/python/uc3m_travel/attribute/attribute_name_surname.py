6fd1b260967e22df5fdebf36dafd7859087c65b4
Pablo Garaulet <100475228@alumnos.uc3m.es>
2024-04-18 11:19:18 +0200

    Codigo duplicado

---
+++ src/main/python/uc3m_travel/attribute/attribute_name_surname.py
@@ -0,0 +1,9 @@
+from .attribute import Attribute
+from ..hotel_management_exception import HotelManagementException
+
+class IdCard(Attribute):
+
+    def __init__(self, name_surname):
+        self._attr_pattern= r"^(?=^.{10,50}$)([a-zA-Z]+(\s[a-zA-Z]+)+)$"
+        self._error_message = "Invalid name format"
+        self._attr_value= self._validate(name_surname)
