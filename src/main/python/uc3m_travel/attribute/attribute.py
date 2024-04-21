6a7532085e47c65797d9377f4715c76396afb985
100475158 <100475158@alumnos.uc3m.es>
2024-04-21 19:05:46 +0200

    versión final de pablo

--- src/main/python/uc3m_travel/attribute/attribute.py
+++ src/main/python/uc3m_travel/attribute/attribute.py
@@ -6,10 +6,10 @@ class Attribute:
     def __init__(self):
         """"Constructor for attributes..."""
         self._attr_pattern = r""
-        self._error_message = ""
+        self._error_message  = ""
         self._attr_value = ""
     def _validate(self, attr_value):
-        coincide = re.compile(self._attr_pattern)
+        coincide= re.compile(self._attr_pattern)
         regex_matches = coincide.fullmatch(attr_value)
         if not regex_matches:
             raise HotelManagementException(self._error_message)
