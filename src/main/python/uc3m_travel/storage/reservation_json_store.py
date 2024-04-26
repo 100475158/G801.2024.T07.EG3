6938bd70d2bbd1da5b344d0a0bc15a797f6d3acd
Pablo Garaulet <100475228@alumnos.uc3m.es>
2024-04-26 17:04:05 +0200

    Singleton hecho

--- src/main/python/uc3m_travel/storage/reservation_json_store.py
+++ src/main/python/uc3m_travel/storage/reservation_json_store.py
@@ -3,19 +3,24 @@ from ..hotel_management_config import JSON_FILES_PATH
 from ..hotel_management_exception import HotelManagementException
 
 class ReservationJsonStore(JsonStore):
-    #class __ReservationJsonStore():
-    def __init__(self):
-        self._file_name= JSON_FILES_PATH + "store_reservation.json"
-        """super().__init__(self._file_name)"""
-        self._data_list= []
-    def add_item(self, item):
-        self.load_store()
-        reservation_found = self.find_item("_HotelReservation__localizer", item.localizer)
-        if reservation_found:
-            raise HotelManagementException("Reservation already exists")
-        reservation_found = self.find_item("_HotelReservation__id_card", item.id_card)
-        if reservation_found:
-            raise HotelManagementException("This ID card has another reservation")
-        super().add_item(item)
+    class __ReservationJsonStore(JsonStore):
+        def __init__(self):
+            self._file_name= JSON_FILES_PATH + "store_reservation.json"
+            """super().__init__(self._file_name)"""
+            self._data_list= []
+        def add_item(self, item):
+            self.load_store()
+            reservation_found = self.find_item("_HotelReservation__localizer", item.localizer)
+            if reservation_found:
+                raise HotelManagementException("Reservation already exists")
+            reservation_found = self.find_item("_HotelReservation__id_card", item.id_card)
+            if reservation_found:
+                raise HotelManagementException("This ID card has another reservation")
+            super().add_item(item)
 
+    __instance = None
 
+    def __new__(cls):
+        if not ReservationJsonStore.__instance:
+            ReservationJsonStore.__instance = ReservationJsonStore.__ReservationJsonStore()
+        return ReservationJsonStore.__instance
